38e84b8f7d913fe326924125129b0fd8be0d17cc
code-Dark-knight <32600576+code-Dark-knight@users.noreply.github.com>
2021-07-07 12:01:21 +0530

    Update del.py

--- client/src/del.py
+++ client/src/del.py
@@ -13,7 +13,7 @@ def remove(params):
 
 def main():
     usagemsg="usage: del [-h] requires key | ex del {key}"
-    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature. As a user, I should be able to perform set(key, val) and get(key)  operations over HTTP and also subscribe to changes happening to any of the keys.',usage=usagemsg)
+    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature.',usage=usagemsg)
     my_parser.add_argument('rem', action='store', nargs=2,type=str)
     args = my_parser.parse_args()
     if args.rem:

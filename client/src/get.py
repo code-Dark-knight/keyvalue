4296624b595c8f40be17a23b875205da771a9601
code-Dark-knight <32600576+code-Dark-knight@users.noreply.github.com>
2021-07-07 12:01:38 +0530

    Update get.py

--- client/src/get.py
+++ client/src/get.py
@@ -13,7 +13,7 @@ def get(params):
 
 def main():
     usagemsg="usage: get [-h] requires key | ex get {key}"
-    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature. As a user, I should be able to perform set(key, val) and get(key)  operations over HTTP and also subscribe to changes happening to any of the keys.',usage=usagemsg)
+    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature.',usage=usagemsg)
     my_parser.add_argument('get', action='store', nargs=2,type=str)
     args = my_parser.parse_args()
     if args.get:

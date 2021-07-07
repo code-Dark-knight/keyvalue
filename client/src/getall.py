2ae656c65663d6df2c1e86ff16733a1d2a19dd36
code-Dark-knight <32600576+code-Dark-knight@users.noreply.github.com>
2021-07-07 12:01:57 +0530

    Update getall.py

--- client/src/getall.py
+++ client/src/getall.py
@@ -12,7 +12,7 @@ def getall():
 
 def main():
     usagemsg="usage: getall [-h] requires key | ex getall"
-    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature. As a user, I should be able to perform set(key, val) and get(key)  operations over HTTP and also subscribe to changes happening to any of the keys.',usage=usagemsg)
+    my_parser = argparse.ArgumentParser(description='A Simple KV (key-value) store web service with a subscription feature.',usage=usagemsg)
     my_parser.add_argument('getall', nargs="*",type=str)
     args = my_parser.parse_args()
     getall()
